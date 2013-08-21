0bc818e318c2b460fe6d17685dd9ec8e37e0959e
Bob Trigg <bobtrigg94930@gmail.com>
2013-08-20 21:40:56 -0700

    Remove db_info files from tracking Add db_info template file

--- includes/db_info.bh.php
+++
@@ -1,6 +0,0 @@
-<?php
-DEFINE('DB_HOST', 'localhost');
-DEFINE('DB_USER','bobtrigg');
-DEFINE('DB_PASS','youcanthavethis');
-DEFINE('DB_NAME', 'bobtrigg_registration');
-?>
--- includes/db_info.bt.php
+++
@@ -1,6 +0,0 @@
-<?php
-DEFINE('DB_HOST', 'localhost');
-DEFINE('DB_USER','bobtrigg');
-DEFINE('DB_PASS','TooAnal123!');
-DEFINE('DB_NAME', 'bobtrigg_calendar');
-?>
